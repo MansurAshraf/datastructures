37d556ce124ed471efa0cca84a60567aa3ca79b7
Mansur Ashraf <mansoor.ashraf@gmail.com>
2013-02-18 23:30:40 -0600

    fixed broken test

--- src/test/java/com/mansoor/datastructures/MergeSortTest.java
+++ src/test/java/com/mansoor/datastructures/MergeSortTest.java
@@ -1,29 +1,28 @@
-package com.mansoor.datastructure;
+package com.mansoor.datastructures;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.fest.assertions.api.Assertions.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MergeSortTest{
-	private MergeSort sort;
-	private static final Logger logger=LoggerFactory.getLogger(MergeSortTest.class);
+import static org.fest.assertions.api.Assertions.assertThat;
 
-	@Before
-	public void setup(){
-		sort=new MergeSort();
-	}
+public class MergeSortTest {
+    private MergeSort sort;
+    private static final Logger logger = LoggerFactory.getLogger(MergeSortTest.class);
 
-	@Test
-	public void testSort(){
-		int[] input=new int[]{1,3,6,4,8,12,5};
-		sort.sort(input);
-		for (int i : input) {
-		 logger.info(String.valueOf(i));	
-		}
-		assertThat(input[2]).isEqualTo(4);
-	
-	}
+    @Before
+    public void setup() {
+        sort = new MergeSort();
+    }
+
+    @Test
+    public void testSort() {
+        int[] input = new int[]{1, 3, 6, 4, 8, 12, 5};
+        sort.sort(input);
+        assertThat(input[2]).isEqualTo(4);
+
+    }
 
 
 }
